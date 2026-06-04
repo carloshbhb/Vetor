0e07e3d5ecf083c680d9b752ac66ee52fbc72b45
Henrique <henrique@exemplo.com>
2026-06-04 12:38:14 -0300

    feat: adiciona Google Analytics 4 com ID G-9D57MBBJN7
    - Adiciona script do GA4 no layout.tsx
    - Atualiza .env.example com ID do GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,6 +109,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9D57MBBJN7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9D57MBBJN7');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
